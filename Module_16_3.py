58e5eac6fecae2fea1c58a9c9fbdf55461c0d83d
Tatiana-E <tanya.era1989@gmail.com>
2024-12-16 10:37:42 +0500

    Update Module_16_3.py

--- Module_16_3.py
+++ Module_16_3.py
@@ -1,8 +1,4 @@
 from fastapi import FastAPI, HTTPException, Path
-
-app = FastAPI()
-
-from fastapi import FastAPI, HTTPException, Path
 from typing import  Annotated
 
 app = FastAPI()
@@ -21,16 +17,18 @@ async def create_user(username: str = Path(min_length=5, max_length=30, descript
     return f"User {new_user_id} is registered"
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id:int, username: str = Path(min_length=5, max_length=30, description="Enter username", examples="Jenny_Smith"),
+async def update_user(user_id: str, username: str = Path(min_length=5, max_length=30, description="Enter username", examples="Jenny_Smith"),
                       age: int = Path(ge=18, le=120, description="Enter age",examples="27")):
     users[user_id] = f"Имя: {username}, возраст: {age}"
     return f"The user {user_id} is updated"
 
 
 @app.delete("/user/{user_id}")
-async def delete_user(user_id: int = Path(ge=1, le=100, description="Enter User ID", examples="1")):
-    for i, user in enumerate(users):
-        if user['id'] == user_id:
-            users.pop(str(user_id))
-            return f"User {user_id} has been deleted"
+async def delete_user(user_id):
+    if user_id in users:
+        users.pop(str(user_id))
+        return f"User {user_id} has been deleted"
+    else:
         raise HTTPException(status_code=404, detail='Users not found')
+
+
